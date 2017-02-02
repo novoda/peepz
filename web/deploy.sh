213eb42bb0ecc213334a800df9752f01d421d3a5
Carl-Gustaf Harroch <carl@novoda.com>
2017-02-02 16:00:43 +0000

    moving to peepz.me

--- web/deploy.sh
+++ web/deploy.sh
@@ -4,4 +4,4 @@ mkdir -p dist/public
 npm run build
 cp index.html dist/public/
 cp assets dist/public/
-cd dist && firebase deploy
+cd dist &&  ../node_modules/.bin/firebase deploy --token=$FIREBASE_TOKEN --non-interactive
