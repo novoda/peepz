76744bffe8d198c9c630d4b5881270cf3dd0848a
Ataul Munim <hello@ataulm.com>
2017-02-05 18:08:36 +0000

    Add drop shadow for sign in button
    - had to switch back to a dirty old asset instead of the shape gradient to get the drop shadow
    - tested it with text size full and density large and it all still fits with the given text

--- android/mobile/src/main/java/com/novoda/peepz/SignInActivity.java
+++ android/mobile/src/main/java/com/novoda/peepz/SignInActivity.java
@@ -3,7 +3,6 @@ package com.novoda.peepz;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
