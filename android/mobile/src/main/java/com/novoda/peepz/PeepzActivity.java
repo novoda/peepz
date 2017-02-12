be8aef569f29e197c7c3e43f3f5c75587a8f4cb9
Ataul Munim <hello@ataulm.com>
2017-02-12 13:39:42 +0000

    Extract PeepzPageDisplayer

--- android/mobile/src/main/java/com/novoda/peepz/PeepzActivity.java
+++ android/mobile/src/main/java/com/novoda/peepz/PeepzActivity.java
@@ -3,9 +3,8 @@ package com.novoda.peepz;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
 
+import com.google.android.cameraview.CameraView;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 import com.novoda.accessibility.AccessibilityServices;
@@ -14,14 +13,11 @@ import com.novoda.support.SystemClock;
 import java.util.Comparator;
 import java.util.List;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PeepzActivity extends BaseActivity {
 
-    @BindView(R.id.peepz_content)
-    PeepzContentView peepzContentView;
-
+    private PeepzPageDisplayer peepzPageDisplayer;
     private AutomaticPictureTaker automaticPictureTaker;
 
     @Override
@@ -30,47 +26,46 @@ public class PeepzActivity extends BaseActivity {
         setContentView(R.layout.activity_peepz);
         ButterKnife.bind(this);
 
-        Toolbar toolbar = ButterKnife.findById(this, R.id.toolbar);
-        final View takePictureButton = ButterKnife.findById(this, R.id.peepz_button_take_picture);
-
-        if (AccessibilityServices.newInstance(this).isSpokenFeedbackEnabled() || !peepzContentView.isInTouchMode()) {
-            toolbar.inflateMenu(R.menu.peepz);
-            toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-                @Override
-                public boolean onMenuItemClick(MenuItem item) {
-                    if (item.getItemId() == R.id.peepz_menu_take_picture) {
-                        onClickTakePicture();
-                        return true;
-                    }
-                    return false;
-                }
-            });
-            takePictureButton.setVisibility(View.GONE);
-        } else {
-            takePictureButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onClickTakePicture();
-                }
-            });
-            takePictureButton.setVisibility(View.VISIBLE);
-        }
+        peepzPageDisplayer = createPageDisplayer();
+        peepzPageDisplayer.bindMenu(callback);
 
         // TODO: what if the user is not signed in?
         FirebaseUser signedInUser = firebaseApi().getSignedInUser();
         PeepUpdater peepUpdater = new PeepUpdater(new SystemClock(), FirebaseDatabase.getInstance(), signedInUser);
         PictureUploader pictureUploader = new PictureUploader(signedInUser);
-        automaticPictureTaker = new AutomaticPictureTaker(peepzContentView.getCameraView(), pictureUploader, peepUpdater);
+        automaticPictureTaker = new AutomaticPictureTaker((CameraView) ButterKnife.findById(this, R.id.peepz_secret_camera), pictureUploader, peepUpdater);
 
         Comparator<Peep> comparator = new PeepCompoundComparator(new SignedInUserIsFirstPeepComparator(signedInUser.getUid()), new LastSeenPeepComparator());
         PeepzService peepzService = new PeepzService(FirebaseDatabase.getInstance(), comparator);
         peepzService.observeChanges(onPeepsUpdatedCallback);
     }
 
+    private PeepzPageDisplayer createPageDisplayer() {
+        return new AndroidPeepzPageDisplayer(
+                AccessibilityServices.newInstance(this),
+                (Toolbar) ButterKnife.findById(this, R.id.toolbar),
+                (PeepzView) ButterKnife.findById(this, R.id.peepz),
+                ButterKnife.findById(this, R.id.peepz_button_take_picture)
+        );
+    }
+
+    private final PeepzPageDisplayer.Callback callback = new PeepzPageDisplayer.Callback() {
+        @Override
+        public void onClickTakePicture() {
+            startActivity(new Intent(PeepzActivity.this, SelfieActivity.class));
+        }
+
+        @Override
+        public void onClickSignOut() {
+            firebaseApi().signOut();
+            finish();
+        }
+    };
+
     private final PeepzService.Callback onPeepsUpdatedCallback = new PeepzService.Callback() {
         @Override
         public void onNext(List<Peep> peepz) {
-            peepzContentView.update(peepz);
+            peepzPageDisplayer.display(peepz);
             FirebaseUser signedInUser = firebaseApi().getSignedInUser();
             if (missingSignedInUserFromPeepz(peepz, signedInUser)) {
                 automaticPictureTaker.requestPictureTake();
@@ -98,10 +93,6 @@ public class PeepzActivity extends BaseActivity {
     protected void onPause() {
         automaticPictureTaker.stop();
         super.onPause();
-    }
-
-    private void onClickTakePicture() {
-        startActivity(new Intent(this, SelfieActivity.class));
     }
 
 }
