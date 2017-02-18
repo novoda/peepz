26bee168295a9498a3cf5d981216e78d0dadb4db
Ataul Munim <hello@ataulm.com>
2017-02-18 15:34:57 +0000

    Remove auto picture taking capability from PictureTaker
    - whoops had it in two places.

--- android/mobile/src/main/java/com/novoda/peepz/PreviewlessPictureTaker.java
+++ android/mobile/src/main/java/com/novoda/peepz/PreviewlessPictureTaker.java
@@ -1,20 +1,14 @@
 package com.novoda.peepz;
 
-import android.os.Handler;
 import android.util.Log;
 
 import com.google.android.cameraview.CameraView;
 
-import java.util.concurrent.TimeUnit;
-
 class PreviewlessPictureTaker {
-
-    private static final int DELAY_MINUTES_BETWEEN_AUTOMATIC_PHOTOS = 2;
 
     private final CameraView cameraView;
     private final PictureUploader pictureUploader;
     private final PeepUpdater peepUpdater;
-    private final Handler handler = new Handler();
 
     private Callback callback;
     private boolean cameraReady;
@@ -32,13 +26,29 @@ class PreviewlessPictureTaker {
         cameraView.start();
     }
 
+    public void takeNewPicture() {
+        if (cameraReady) {
+            log("takeNewPicture");
+            cameraView.takePicture();
+        } else {
+            log("takeNewPicture but camera not ready");
+        }
+    }
+
+    public void stop() {
+        cameraReady = false;
+        cameraView.stop();
+        cameraView.removeCallback(cameraViewCallback);
+
+        this.callback = null;
+    }
+
     private final CameraView.Callback cameraViewCallback = new CameraView.Callback() {
 
         @Override
         public void onCameraOpened(CameraView cameraView) {
             log("onCameraOpened");
             cameraReady = true;
-            scheduleAutomaticPictureTake();
         }
 
         @Override
@@ -63,40 +73,9 @@ class PreviewlessPictureTaker {
         public void onCameraClosed(CameraView cameraView) {
             log("onCameraClosed");
             cameraReady = false;
-            handler.removeCallbacks(runnable);
         }
 
     };
-
-    private void scheduleAutomaticPictureTake() {
-        long delayMillis = TimeUnit.MINUTES.toMillis(DELAY_MINUTES_BETWEEN_AUTOMATIC_PHOTOS);
-        handler.postDelayed(runnable, delayMillis);
-    }
-
-    private final Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            takeNewPicture();
-            scheduleAutomaticPictureTake();
-        }
-    };
-
-    public void takeNewPicture() {
-        if (cameraReady) {
-            log("takeNewPicture");
-            cameraView.takePicture();
-        } else {
-            log("takeNewPicture but camera not ready");
-        }
-    }
-
-    public void stop() {
-        cameraReady = false;
-        cameraView.stop();
-        cameraView.removeCallback(cameraViewCallback);
-
-        this.callback = null;
-    }
 
     public interface Callback {
 
