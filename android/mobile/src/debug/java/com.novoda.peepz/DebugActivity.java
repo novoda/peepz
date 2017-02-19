111850a184122c4e326d2522c1cf315f4c629f44
Ataul Munim <hello@ataulm.com>
2017-02-19 23:27:55 +0000

    Add activated states to radio buttons

--- android/mobile/src/debug/java/com.novoda.peepz/DebugActivity.java
+++ android/mobile/src/debug/java/com.novoda.peepz/DebugActivity.java
@@ -3,6 +3,7 @@ package com.novoda.peepz;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.widget.Toast;
 
 public class DebugActivity extends BaseActivity {
 
@@ -13,11 +14,26 @@ public class DebugActivity extends BaseActivity {
     }
 
     public void openDialog(View view) {
-        View dialogView = getLayoutInflater().inflate(R.layout.debug_view_dialog, null, false);
-        new AlertDialog.Builder(this)
+        SettingsDialogWidget dialogView = (SettingsDialogWidget) getLayoutInflater().inflate(R.layout.debug_view_dialog, null, false);
+        final AlertDialog alertDialog = new AlertDialog.Builder(this)
                 .setView(dialogView)
-                .create()
-                .show();
+                .create();
+
+        dialogView.bind(new SettingsDialogWidget.Callback() {
+            @Override
+            public void onClickOk(PictureTakeInterval interval) {
+                Toast.makeText(getApplicationContext(), "click: " + interval.name(), Toast.LENGTH_SHORT).show();
+                alertDialog.dismiss();
+            }
+
+            @Override
+            public void onClickCancel() {
+                alertDialog.dismiss();
+            }
+        }, PictureTakeInterval.FREQUENT);
+
+        alertDialog.show();
     }
+
 
 }
--- android/mobile/src/main/java/com/novoda/peepz/SettingsDialogWidget.java
+++ android/mobile/src/main/java/com/novoda/peepz/SettingsDialogWidget.java
@@ -1,18 +1,33 @@
 package com.novoda.peepz;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.support.design.internal.ForegroundLinearLayout;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SettingsDialogWidget extends LinearLayout {
+
+    @BindView(R.id.settings_dialog_timer_frequent)
+    View frequentTimerView;
+
+    @BindView(R.id.settings_dialog_timer_infrequent)
+    View infrequentTimerView;
+
+    @BindView(R.id.settings_dialog_timer_off)
+    View offTimerView;
+
+    @BindView(R.id.settings_dialog_button_ok)
+    View okButton;
+
+    @BindView(R.id.settings_dialog_button_cancel)
+    View cancelButton;
 
     public SettingsDialogWidget(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -29,6 +44,102 @@ public class SettingsDialogWidget extends LinearLayout {
         super.onFinishInflate();
         View.inflate(getContext(), R.layout.merge_settings_dialog_widget, this);
         ButterKnife.bind(this);
+    }
+
+    public void bind(final Callback callback, PictureTakeInterval pictureTakeInterval) {
+        updateActivatedStateOnTimerViews(pictureTakeInterval);
+        updateClickListenersForTimerViews();
+
+        okButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (View view : timerViews()) {
+                    if (view.isActivated()) {
+                        PictureTakeInterval interval = getPictureTakeIntervalAssociatedWith(view);
+                        callback.onClickOk(interval);
+                        break;
+                    }
+                }
+            }
+        });
+
+        cancelButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callback.onClickCancel();
+            }
+        });
+    }
+
+    private void updateClickListenersForTimerViews() {
+        frequentTimerView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateActivatedStateOnTimerViews(PictureTakeInterval.FREQUENT);
+            }
+        });
+
+        infrequentTimerView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateActivatedStateOnTimerViews(PictureTakeInterval.INFREQUENT);
+            }
+        });
+
+        offTimerView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateActivatedStateOnTimerViews(PictureTakeInterval.OFF);
+            }
+        });
+    }
+
+
+    private void updateActivatedStateOnTimerViews(PictureTakeInterval interval) {
+        switch (interval) {
+            case FREQUENT:
+                frequentTimerView.setActivated(true);
+                infrequentTimerView.setActivated(false);
+                offTimerView.setActivated(false);
+                break;
+            case INFREQUENT:
+                frequentTimerView.setActivated(false);
+                infrequentTimerView.setActivated(true);
+                offTimerView.setActivated(false);
+                break;
+            case OFF:
+                frequentTimerView.setActivated(false);
+                infrequentTimerView.setActivated(false);
+                offTimerView.setActivated(true);
+                break;
+            default:
+                throw new IllegalArgumentException("unknown interval: " + interval);
+        }
+    }
+
+    private PictureTakeInterval getPictureTakeIntervalAssociatedWith(View view) {
+        switch (view.getId()) {
+            case R.id.settings_dialog_timer_frequent:
+                return PictureTakeInterval.FREQUENT;
+            case R.id.settings_dialog_timer_infrequent:
+                return PictureTakeInterval.INFREQUENT;
+            case R.id.settings_dialog_timer_off:
+                return PictureTakeInterval.OFF;
+            default:
+                throw new IllegalArgumentException("no PictureTakeInterval associated with view");
+        }
+    }
+
+    private List<View> timerViews() {
+        return Arrays.asList(frequentTimerView, infrequentTimerView, offTimerView);
+    }
+
+    public interface Callback {
+
+        void onClickOk(PictureTakeInterval interval);
+
+        void onClickCancel();
+
     }
 
 }
