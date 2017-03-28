059f91c6f670be9905e8449d16e00df73c3e860a
Adam Brown <adam@novoda.com>
2017-03-28 13:23:54 +0100

    adding basic ui for joining a room by id

--- web/src/onboarding/onboardingView.js
+++ web/src/onboarding/onboardingView.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class OnboardingView extends React.Component {
 
-  render() {
-    return (<div>You are not a member of any rooms!</div>);
+  constructor(props) {
+    super(props);
+    this.state = {canSubmit: false, textFieldValue: ''};
   }
 
-  componentDidMount() {
-    this.props.joinRoom('novoda');
+  render() {
+    return (
+      <div>
+        <div>You are not a member of any rooms, lets fix that!</div>
+        <TextField
+           value={this.state.textFieldValue}
+           hintText="Room ID"
+           floatingLabelText="Room ID"
+           onChange={this._handleTextFieldChange(this)} />
+        <RaisedButton onClick={this._handleSubmit(this)} label="JOIN" disabled={!this.state.canSubmit} />
+      </div>
+    );
+  }
+
+  _handleTextFieldChange(self) {
+    return function(event) {
+      const newText = event.target.value;
+      self.setState({
+          canSubmit: newText && newText.length > 1,
+          textFieldValue: newText
+      });
+    };
+  }
+
+  _handleSubmit(self) {
+    return function(event) {
+      self.props.joinRoom(self.state.textFieldValue);
+    };
   }
 
 }
