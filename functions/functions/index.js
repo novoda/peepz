034b82435d906be21ae5f9e64ae0c08997db7451
Adam Brown <adam@novoda.com>
2017-03-28 08:51:23 +0100

    adding basic function for joining a room

---
+++ functions/functions/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.joinRoom = functions.database.ref('/wip/events/{key}')
+    .onWrite(event => {
+      const data = event.data.val();
+      const user = data.payload.user;
+      const roomId = data.payload.roomId;
+      const wallPath = `wip/rooms/${roomId}/wall`;
+
+      if (data.type === 'JOIN') {
+        return event.data.adminRef.root
+          .child(`/wip/users/${user.uid}/rooms/${roomId}/id`)
+          .set(roomId)
+          .then(updateUser(event.data.adminRef.root)(wallPath)(user));
+      }
+      return;
+    });
+
+const updateUser = ref => wallPath => user => {
+  return ref(`${wallPath}/${user.uid}`).set({
+    uid: user.uid,
+    name: user.displayName
+  });
+};
