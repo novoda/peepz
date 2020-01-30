6ad17e46f97aac7f6ca8c8bfce9de722f2840727
Carl Harroch <carl@novoda.com>
2020-01-30 13:22:37 +0000

    up firebase functions and deployed a join the room functionality

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,40 +1,52 @@
-'use strict';
+'use strict'
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
 
-exports.joinRoom = functions.database.ref('/wip/events/{key}')
-    .onWrite(event => {
-      const data = event.data.val();
+// exports.joinRoom = functions.database.ref('/wip/events/{key}').onWrite(event => {
+//   const data = event.data.val()
 
-      if (data.type === 'JOIN') {
-        const user = data.payload.user;
-        const roomId = data.payload.roomId;
-        const wallPath = `wip/rooms/${roomId}/wall`;
-        return event.data.adminRef.root
-          .child(`/wip/users/${user.uid}/rooms/${roomId}/id`)
-          .set(roomId)
-          .then(updateUser(event.data.adminRef.root)(wallPath)(user));
-      }
+//   if (data.type === 'JOIN') {
+//     const user = data.payload.user
+//     const roomId = data.payload.roomId
+//     const wallPath = `wip/rooms/${roomId}/wall`
+//     return event.data.adminRef.root
+//       .child(`/wip/users/${user.uid}/rooms/${roomId}/id`)
+//       .set(roomId)
+//       .then(updateUser(event.data.adminRef.root)(wallPath)(user))
+//   }
 
-      return;
-    });
+//   return
+// })
 
-const updateUser = ref => wallPath => user => {
-  return ref.child(`${wallPath}/${user.uid}`).set({
-    uid: user.uid,
-    name: user.displayName
-  });
-};
+// const updateUser = ref => wallPath => user => {
+//   return ref.child(`${wallPath}/${user.uid}`).set({
+//     uid: user.uid,
+//     name: user.displayName
+//   })
+// }
 
-exports.createUser = functions.auth.user().onCreate(event => {
-  const user = event.data;
-  return admin.database()
-    .ref(`/wip/users/${user.uid}/profile/`)
+// exports.createUser = functions.auth.user().onCreate(event => {
+//   const user = event.data
+//   return admin
+//     .database()
+//     .ref(`/wip/users/${user.uid}/profile/`)
+//     .set({
+//       displayName: user.displayName,
+//       email: user.email,
+//       uid: user.uid
+//     })
+// })
+
+// `wip/users/{user.uid}/rooms/novoda/id`
+exports.updateRoomId = functions.auth.user().onCreate(user => {
+  // if(user.email.con)
+  // console.log(user)
+  return admin
+    .database()
+    .ref(`wip/users/${user.uid}/rooms/novoda`)
     .set({
-      displayName: user.displayName,
-      email: user.email,
-      uid: user.uid
-    });
-});
+      id: 'novoda'
+    })
+})
