ee12adcd67148b443c8683455fbf72378cdc3de1
Carl Harroch <carl@novoda.com>
2020-02-11 10:44:06 +0000

    working and tested geofence

--- android/mobile/src/main/java/com/novoda/peepz/BaseActivity.java
+++ android/mobile/src/main/java/com/novoda/peepz/BaseActivity.java
@@ -1,13 +1,25 @@
 package com.novoda.peepz;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import androidx.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
@@ -16,6 +28,7 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     private FirebaseApi firebaseApi;
     private GoogleApiClientApi googleApiClientApi;
+    private GeofencingClient geofencingClient;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,6 +36,64 @@ public abstract class BaseActivity extends AppCompatActivity {
         googleApiClientApi = new GoogleApiClientApi(this);
         googleApiClientApi.setupGoogleApiClient();
         firebaseApi = new FirebaseApi(FirebaseAuth.getInstance(), googleApiClientApi);
+
+        geofencingClient = LocationServices.getGeofencingClient(this);
+
+        Log.e("TEST", "addGeofence");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.e("TEST", "Can't check for permission");
+            return;
+        }
+        geofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent())
+                .addOnSuccessListener(this, aVoid -> {
+                    Log.i("TEST", "We have entered something worth considering");
+                })
+                .addOnFailureListener(this, e -> {
+                    Log.e("TEST", "Error");
+                });
+    }
+
+    private GeofencingRequest getGeofencingRequest() {
+        List geofenceList = new ArrayList();
+        geofenceList.add(new Geofence.Builder()
+                .setRequestId("Carl_home")
+                .setCircularRegion(
+                        Double.parseDouble("51.5446"),
+                        Double.parseDouble("-0.0358"),
+                        50
+                )
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                        Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build());
+
+
+        geofenceList.add(new Geofence.Builder()
+                .setRequestId("novoda_unit8")
+                .setCircularRegion(
+                        51.5415,
+                        -0.0956,
+                         50
+                )
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                        Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build());
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(geofenceList);
+        return builder.build();
+    }
+
+    private PendingIntent geofencePendingIntent = null;
+    private PendingIntent getGeofencePendingIntent() {
+        if (geofencePendingIntent != null) {
+            return geofencePendingIntent;
+        }
+        Intent intent = new Intent(this, CheckInReceiver.class);
+        geofencePendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+        return geofencePendingIntent;
     }
 
     protected FirebaseApi firebaseApi() {
--- android/mobile/src/main/java/com/novoda/peepz/CheckInReceiver.kt
+++ android/mobile/src/main/java/com/novoda/peepz/CheckInReceiver.kt
@@ -1,11 +1,38 @@
 package com.novoda.peepz
 
-import android.os.Bundle
-import com.google.android.gms.location.GeofencingClient
-import com.google.android.gms.location.LocationServices
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.google.android.gms.location.GeofencingEvent
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
 
-lateinit var geofencingClient: GeofencingClient
+class CheckInReceiver : BroadcastReceiver() {
 
-override fun onCreate(savedInstanceState: Bundle?) {
-    geofencingClient = LocationServices.getGeofencingClient(this)
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val db = FirebaseFirestore.getInstance()
+        val auth = FirebaseAuth.getInstance().currentUser
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+        if (geofencingEvent.hasError()) {
+            val errorMessage = GeofenceStatusCodes.getStatusCodeString(geofencingEvent.errorCode)
+            Log.e("TEST", errorMessage)
+            return
+        }
+        val geofenceTransition = geofencingEvent.geofenceTransition
+        if ((geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) or (geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT)) {
+            val triggeringGeofences = geofencingEvent.triggeringGeofences
+            db.collection("peepz/${auth?.uid}/places").add(hashMapOf(
+                    "transition" to geofencingEvent.geofenceTransition,
+                    "place" to triggeringGeofences[0].requestId,
+                    "when" to FieldValue.serverTimestamp(),
+                    "position" to GeoPoint(geofencingEvent.triggeringLocation.latitude, geofencingEvent.triggeringLocation.longitude)
+            ))
+        } else {
+        }
+    }
 }
