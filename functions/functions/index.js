5f15cf20b404eeee1967c75adcf9b66b2c69135f
Carl Harroch <carl@novoda.com>
2020-02-16 14:04:55 +0000

    adidng geolocation

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -4,61 +4,15 @@ const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const { Storage } = require('@google-cloud/storage')
 
-admin.initializeApp(functions.config().firebase)
+if (process.env.FIREBASE_CONFIG) {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://peepz-2f400.firebaseio.com'
+  })
+} else {
+  admin.initializeApp(functions.config().firebase)
+}
 
-// Creates a client
-// const storage = new Storage()
-
-// async function listFiles() {
-//   // Lists files in the bucket
-//   const [files] = await storage
-//     .bucket('gs://peepz-2f400.appspot.com/')
-//     .getFiles({ directory: 'wip/rooms/novoda/wall/vTtdPbyVBTMVKdLAlyP1fcSUvvS2', versions: true })
-
-//   console.log('Files:')
-//   files.forEach(file => {
-//     console.log(file.generation)
-//   })
-// }
-
-// listFiles().catch(console.error)
-
-// exports.joinRoom = functions.database.ref('/wip/events/{key}').onWrite(event => {
-//   const data = event.data.val()
-
-//   if (data.type === 'JOIN') {
-//     const user = data.payload.user
-//     const roomId = data.payload.roomId
-//     const wallPath = `wip/rooms/${roomId}/wall`
-//     return event.data.adminRef.root
-//       .child(`/wip/users/${user.uid}/rooms/${roomId}/id`)
-//       .set(roomId)
-//       .then(updateUser(event.data.adminRef.root)(wallPath)(user))
-//   }
-
-//   return
-// })
-
-// const updateUser = ref => wallPath => user => {
-//   return ref.child(`${wallPath}/${user.uid}`).set({
-//     uid: user.uid,
-//     name: user.displayName
-//   })
-// }
-
-// exports.createUser = functions.auth.user().onCreate(event => {
-//   const user = event.data
-//   return admin
-//     .database()
-//     .ref(`/wip/users/${user.uid}/profile/`)
-//     .set({
-//       displayName: user.displayName,
-//       email: user.email,
-//       uid: user.uid
-//     })
-// })
-
-// `wip/users/{user.uid}/rooms/novoda/id`
 exports.updateRoomId = functions.auth.user().onCreate(user => {
   // if(user.email.con)
   return admin
@@ -66,5 +20,18 @@ exports.updateRoomId = functions.auth.user().onCreate(user => {
     .ref(`wip/users/${user.uid}/rooms/novoda`)
     .set({
       id: 'novoda'
+    })
+})
+
+exports.updateLocation = functions.firestore.document('/peepz/{uid}/places/{placeId}').onWrite((change, context) => {
+  const uid = context.params.uid
+  const place = change.after.data()
+  const isEntereing = place.transition == 1
+  const name = place.place || ''
+  return admin
+    .database()
+    .ref(`wip/rooms/novoda/wall/${uid}`)
+    .update({
+      place: name
     })
 })
--- web/src/store.js
+++ web/src/store.js
@@ -1,30 +1,28 @@
-import reducer from './reducer';
-import { compose, createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import loggerMiddleware from './loggerMiddlewear';
-import firebaseMiddleware from './firebaseMiddlewear';
-import wallMiddleware from './middleware/wallMiddleware';
-import authMiddleware from './middleware/authMiddleware';
-import roomOptionsMiddleware from './middleware/roomOptionsMiddleware';
-import userRoomOptionsMiddleware from './middleware/userRoomOptionsMiddleware';
-import joinRoomMiddleware from './middleware/joinRoomMiddleware';
+import reducer from './reducer'
+import { compose, createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import loggerMiddleware from './loggerMiddlewear'
+import firebaseMiddleware from './firebaseMiddlewear'
+import wallMiddleware from './middleware/wallMiddleware'
+import authMiddleware from './middleware/authMiddleware'
+import roomOptionsMiddleware from './middleware/roomOptionsMiddleware'
+import userRoomOptionsMiddleware from './middleware/userRoomOptionsMiddleware'
+import joinRoomMiddleware from './middleware/joinRoomMiddleware'
 
-const enhancer = firebase => compose(
-  applyMiddleware(
-    thunk,
-    loggerMiddleware,
-    firebaseMiddleware(firebase),
-    wallMiddleware(firebase),
-    authMiddleware(firebase),
-    roomOptionsMiddleware(firebase),
-    userRoomOptionsMiddleware(firebase),
-    joinRoomMiddleware(firebase)
+const enhancer = firebase =>
+  compose(
+    applyMiddleware(
+      thunk,
+      // loggerMiddleware,
+      firebaseMiddleware(firebase),
+      wallMiddleware(firebase),
+      authMiddleware(firebase),
+      roomOptionsMiddleware(firebase),
+      userRoomOptionsMiddleware(firebase),
+      joinRoomMiddleware(firebase)
+    )
   )
-);
 
 export function create(firebase) {
-  return createStore(
-    reducer,
-    enhancer(firebase)
-  );
+  return createStore(reducer, enhancer(firebase))
 }
