5a51b5f9fba9a1429843639410eb686197db6937
Michal <michal@novoda.com>
2020-09-23 11:10:10 +0100

    Added clean iOS Project

---
+++ ios/Peepz/Peepz/PeepzApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeepzApp.swift
+//  Peepz
+//
+//  Created by Michal Novoda on 23/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct PeepzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
