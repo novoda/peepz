215d41276dff99d039de1bf91621ffdd60b29b22
Michal <michal@novoda.com>
2020-09-28 15:12:52 +0100

    Configured firebase

--- ios/Peepz/Peepz/PeepzApp.swift
+++ ios/Peepz/Peepz/PeepzApp.swift
@@ -6,9 +6,19 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct PeepzApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
