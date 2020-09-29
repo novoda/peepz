22b79796bd3c8738e29d7e35bfd907d2cc5085d8
Michal <michal@novoda.com>
2020-09-29 11:19:54 +0100

    Added support for mac

--- ios/Peepz/Peepz/PeepzApp.swift
+++ ios/Peepz/Peepz/PeepzApp.swift
@@ -25,3 +25,9 @@ struct PeepzApp: App {
         }
     }
 }
+
+struct PeepzApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
