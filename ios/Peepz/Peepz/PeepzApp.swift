cf96b042fb09dd9ce02d4e030ac4ed9badb32d8c
Michal <michal@novoda.com>
2020-10-01 11:17:18 +0100

    Commented out Firebase code.

--- ios/Peepz/Peepz/PeepzApp.swift
+++ ios/Peepz/Peepz/PeepzApp.swift
@@ -1,33 +1,20 @@
-//
-//  PeepzApp.swift
-//  Peepz
-//
-//  Created by Michal Novoda on 23/09/2020.
-//
-
 import SwiftUI
-import Firebase
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        return true
-    }
-}
+//import Firebase
+//
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//        FirebaseApp.configure()
+//        return true
+//    }
+//}
 
 @main
 struct PeepzApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    // @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-    }
-}
-
-struct PeepzApp_Previews: PreviewProvider {
-    static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
